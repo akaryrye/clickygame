1999b2afe6c9ac0bbabfbf644aac8b06f74af42e
akaryrye <ralldrin@gmail.com>
2019-10-29 20:11:34 -0700

    Updates

---
+++ precache-manifest.687bf83ce0663fe8c75d22a9826fa644.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "398869909461da950988a6a783c09a6d",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "1d4716db9bd44a3c1721",
+    "url": "/clickygame/static/css/main.782e7f37.chunk.css"
+  },
+  {
+    "revision": "7953f95efca1b0977526",
+    "url": "/clickygame/static/js/2.629f8e5c.chunk.js"
+  },
+  {
+    "revision": "1d4716db9bd44a3c1721",
+    "url": "/clickygame/static/js/main.1aecd436.chunk.js"
+  },
+  {
+    "revision": "9e617109b3f2d0aedd5b",
+    "url": "/clickygame/static/js/runtime-main.22c9b625.js"
+  }
+]);
